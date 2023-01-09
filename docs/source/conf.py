ce1627d13f35b155789d3750d8332315ef9c969a
Scott Huberty <52462026+scott-huberty@users.noreply.github.com>
2023-01-09 13:27:30 -0500

    initial commit

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Quebec 1000 Protocol'
+copyright = '2023, Scott Huberty'
+author = 'Scott Huberty'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
+html_logo = "tacc-logo.png"
+html_theme_options = {
+    "announcement": "<p>Here's an Announcemnet!</p>",
+}
---
+++ docs/source/contribute.rst
@@ -0,0 +1,16 @@
+Contributing
+============
+
+The Q1K Protocol documentation is built using `Sphinx`, and version control
+is managed by `git`. Thus, to make changes to the documentation, you need to
+clone the `github repo <https://github.com/scott-huberty/Q1K-protocol/>`_`
+
+Installation
+------------
+First, clone the repository using git: 
+
+.. code-block:: console
+    
+    (.venv) $ git clone https://github.com/scott-huberty/Q1K-protocol
+
+
